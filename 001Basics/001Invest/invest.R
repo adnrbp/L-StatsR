ed3d1a054c6d174ba5458a991c826c1ad0c93213
adnrbp <adnrbp@users.noreply.github.com>
2018-11-25 21:22:43 -0500

    variables - basic value assignment

---
+++ 001Basics/001Invest/invest.R
@@ -0,0 +1,10 @@
+savings <- 100
+growth_multiplier <- 1.1
+years <- 7
+
+result <- savings * growth_multiplier^years
+result
+
+profit <- FALSE
+desc <- "you have invested some amount of money"
+#Rscript invest.R
