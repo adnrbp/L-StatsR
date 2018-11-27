0c5958a8bdd25af4a9327a670c402aba8bb8bbb1
adnrbp <adnrbp@users.noreply.github.com>
2018-11-26 22:07:59 -0500

    Vectors -eq Return of investments

--- 001Basics/001Invest/invest.R
+++ 001Basics/001Invest/invest.R
@@ -1,3 +1,4 @@
+#Rscript invest.R
 savings <- 100
 growth_multiplier <- 1.1
 years <- 7
@@ -7,4 +8,16 @@ result
 
 profit <- FALSE
 desc <- "you have invested some amount of money"
-#Rscript invest.R
+
+week_days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+#percent:
+apple_eq_returns <- c(1,-2,3,1,-2)
+nvidia_eq_returns <- c(4,-1,2,3,1)
+names(apple_eq_returns) <- week_days
+names(nvidia_eq_returns) <- week_days
+
+apple_eq_returns
+nvidia_eq_returns
+
+total_return <- apple_eq_returns + nvidia_eq_returns
+total_return
