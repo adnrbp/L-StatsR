3f0e639417c4e47b9ca1a17899a1ec9889ee70fc
adnrbp <adnrbp@users.noreply.github.com>
2018-11-27 22:12:57 -0500

    Return Multiplier - diferent Months

--- 001Basics/001Invest/invest.R
+++ 001Basics/001Invest/invest.R
@@ -21,3 +21,22 @@ nvidia_eq_returns
 
 total_return <- apple_eq_returns + nvidia_eq_returns
 total_return
+
+
+#base information
+interest_rate <- 10 #percent per month
+starting_cash <- 50 #dolars
+
+#Return multiplier
+mult <- 1 + interest_rate / 100
+
+#new amount
+new_cash <- starting_cash * mult
+
+jan_mult <- mult
+feb_interest <- 5
+feb_mult <-  1 + feb_interest / 100
+
+#multiple periods:
+#10% return + 5% return  (diff months)
+new_cash <- starting_cash * jan_mult * feb_mult
