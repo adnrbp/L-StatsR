ee5b3e93b6341e2e22f67022e93c320638915480
adnrbp <adnrbp@users.noreply.github.com>
2018-11-29 21:24:16 -0500

    adding a matrix for same period stock prices

--- 001Basics/002Stocks/stocks.R
+++ 001Basics/002Stocks/stocks.R
@@ -3,6 +3,7 @@ apple_stocks <- c(109.49,109.90,109.11,109.95,111.03,112.12,113.95,113.30,115.19
 ibm_stocks <- c(159.82, 160.02, 159.84)
 
 
+
 #plot(apple_stocks)
 #plot(apple_stocks, type ="p") #plot points
 plot(apple_stocks, type ="l") #plot lines
@@ -31,4 +32,60 @@ sony_weight <- .8
 portf_2_ret <- microsoft_ret * microsoft_weight +
 				sony_ret * sony_weight
 portf_2_ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Weights, returns, and company names
+ret <- c(7, 9)
+weight <- c(.2, .8)
+companies <- c("Microsoft", "Sony")
+
+# Assign company names to your vectors
+names(ret) <- companies
+names(weight) <- companies
+
+# Multiply the returns and weights together 
+ret_X_weight <- ret * weight
+
+# Print ret_X_weight
+ret_X_weight
+
+# Sum to get the total portfolio return
+portf_ret <-sum(ret_X_weight)
+
+# Print portf_ret
+portf_ret
+
+
+
+
+
+
+
+#stock prices: same time period
+microsoft <- c(59.20,59.25,60.22,59.95)
+ebay <- c(17.44, 18.32, 19.11, 18.22)
+
+#create 2 columns (for each company)
+cbind(microsoft, ebay)
+#bind by rows
+rbind(microsoft,ebay)
+
+#relationship between 2 stocks (how much they move relative to each other)
+cor(microsoft, ebay)
+microsoft_ebay_matrix <- cbind(microsoft, ebay)
+cor(microsoft_ebay_matrix)
+
+
 
