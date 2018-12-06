36686b7085d3a89dd2e2210e2dd1b3349e3787c2
adnrbp <adnrbp@users.noreply.github.com>
2018-12-05 21:54:51 -0500

    define Credit Bonds

---
+++ 002Interm/times.R
@@ -0,0 +1,17 @@
+Sys.Date()
+Sys.Time()
+
+great_crash <- as.Date("1929-11-29")
+
+#date of largest stock market point drop (in a day)
+crash <- as.Date("2008-09-29")
+numDate <- as.numeric(crash)
+#seconds since jan 1 1970
+as.numeric(Sys.time())
+
+
+
+
+dates <- c("2017-01-01", "2017-01-02",
+           "2017-01-03", "2017-01-04") 
+as.Date(dates)
