03749c3c68b2d0b1820609f6a5baf7a8864758d7
adnrbp <adnrbp@users.noreply.github.com>
2018-12-09 23:29:04 -0500

    split cash flow by company and modify differently each company cash flows

--- 001Basics/003CashFlow/BusinessCash.R
+++ 001Basics/003CashFlow/BusinessCash.R
@@ -21,3 +21,29 @@ present_value <- cash_flow * (1 + interest /100) ^ - periods
 print(present_value)
 
 
+
+cash$present_value <-cash$cash_flow * (1 + .05)^-cash$year
+total_pv <- sum(cash$present_value)
+
+#Filtering company B and calculate total present Value (individual contribution)
+cash_B <- subset(cash,company=="B")
+total_pv_B <- sum(cash_B$present_value)
+
+#split cash data frame by company in a new list
+grouping <- cash$company
+split_cash_companies <- split(cash, grouping)
+
+#modify each company cash flow differently
+split_cash_companies$A$cash_flow <- split_cash_companies$A$cash_flow * 2
+split_cash_companies$B$cash_flow <- split_cash_companies$B$cash_flow * 3
+
+new_cash <- unsplit(split_cash_companies, grouping)
+
+
+#split cash data frame by  year in a new list
+grouping2 <- cash$year
+split_cash_years <- split(cash, grouping2)
+#mix the list again
+original_cash <- unsplit(split_cash_years,grouping2)
+
+
