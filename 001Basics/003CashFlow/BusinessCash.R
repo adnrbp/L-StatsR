6e359ba6079bcb8e81195b0cd852d27ce6e3ead1
adnrbp <adnrbp@users.noreply.github.com>
2018-12-10 22:44:57 -0500

    define conditions to buy and sell stocks

--- 001Basics/003CashFlow/BusinessCash.R
+++ 001Basics/003CashFlow/BusinessCash.R
@@ -1,6 +1,6 @@
 company <- c("A", "A", "A", "B", "B", "B", "B")
 cash_flow <- c(1000, 4000, 550, 1500, 1100, 750, 6000)
-year <- c(1, 3, 4, 1, 2, 4, 5
+year <- c(1, 3, 4, 1, 2, 4, 5)
 
 cash <- data.frame(company, cash_flow, year)
 head(cash,n=4)
---
+++ 002Interm/conditionalStocks.R
@@ -0,0 +1,20 @@
+microsoft <- c(59.20,61.25,60.22,62.95)
+ebay <- c(17.44, 18.32, 19.11, 18.22)
+
+dates <- as.Date(c("2017-01-02", "2017-05-03", "2017-08-04", "2017-10-17"))
+
+stocks <- data.frame(dates, microsoft,ebay)
+
+
+#buy ebay stocks if crosses below 18
+stocks$ebay_buy <- stocks$ebay < 18
+
+print(stocks)
+
+#sell microsoft stocks if crosses above 61
+stocks$microsoft_buy <- stocks$microsoft > 61 
+
+
+
+
+
