5048b03eb8d527550a9ab55eb489100d73789098
adnrbp <adnrbp@users.noreply.github.com>
2019-01-05 18:17:49 -0500

    ggplot2 visuzlizations - log scale, aesthetics and sub-graphs

--- 003Libs/001Tidyverse/tidy.R
+++ 003Libs/001Tidyverse/tidy.R
@@ -20,5 +20,23 @@ gapminder %>% mutate(lifeExpMonths = lifeExp * 12)
 gapminder %>% filter(year == 2007) %>% mutate(lifeExpMonths = 12 * lifeExp) %>%  arrange(desc(lifeExpMonths))
 
 
+#visualize
+library(ggplot2)
+
+gapminder_1952 <- gapminder %>% filter(year == 1952)
+
+#start point, each=country
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) +
+  geom_point()
+#life expect distributed
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) + 
+  geom_point()
+
+#using logarithmic scale
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) + 
+  geom_point() + scale_x_log10()
+
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) + 
+  geom_point() + scale_x_log10() + scale_y_log10()
 
 
