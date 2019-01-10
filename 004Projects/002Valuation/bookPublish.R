1f7cfb4b8be8b87111684398483e9fd3957d1d5c
adnrbp <adnrbp@users.noreply.github.com>
2019-01-09 22:29:29 -0500

    valuation - publish own book project, function for gross Profit

--- 004Projects/002Valuation/bookPublish.R
+++ 004Projects/002Valuation/bookPublish.R
@@ -20,3 +20,20 @@ cashflow$gross_profit <- cashflow$revenue - cashflow$direct_expense
 
 # print cashflow
 print(cashflow)
+
+
+#automate with function
+calc_business_model <- function(assumptions, price, print_cost, ship_cost){
+    cashflow <- assumptions
+    cashflow$revenue <- cashflow$sales * price
+    cashflow$direct_expense <- cashflow$sales * (print_cost + ship_cost) 
+    cashflow$gross_profit <- cashflow$revenue - cashflow$direct_expense
+    cashflow
+}
+
+calc_business_model(book_assumptions, 20, 0.5, 2)$gross_profit
+calc_business_model(book_assumptions, 25, 0.5, 2)$gross_profit
+
+
+
+
